d7b2bf83f6b8d72c5e444f4da98f25f886e632a9
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-09-19 10:27:28 +0700

    insert delete update sort

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 var mongo = require("mongodb");
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-var options = {useUnifiedTopology: true,useNewUrlParser:true};
+var options = { useUnifiedTopology: true, useNewUrlParser: true };
 
-
+///////////////////////////Select/////////////////////////////////////
 
 // MongoClient.connect(url,options, function(err, db) {
 //   if (err) throw err;
@@ -28,17 +28,150 @@ var options = {useUnifiedTopology: true,useNewUrlParser:true};
 //   });
 
 
-  MongoClient.connect(url,options, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("coc");
-    var query = {subject_id:"977-341"};
-    dbo.collection("classroom")
-    .find(query)
-    .toArray(function(err, result) {
-      if (err) throw err;
-      console.log(result);
-      db.close();
-    });
-  });
+// MongoClient.connect(url,options, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//   var query = {subject_id:"977-341"};
+//   dbo.collection("classroom")
+//   .find(query)
+//   .toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
 
-  
+// MongoClient.connect(url, options, function (err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//   var query = { subject_id: "140-391" };
+//   dbo.collection("classroom")
+//     .find(query)
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//       db.close();
+//     });
+// });
+
+/////////////////////like/////////////////////
+// MongoClient.connect(url, options, function (err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//   var query = { subject_id: /^140/ };
+//   dbo.collection("classroom")
+//     .find(query)
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//       db.close();
+//     });
+// });
+
+/////////////////////sort order////////////////////
+// MongoClient.connect(url, options, function (err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//   var query = { 
+//     subject_id: /^140/ 
+//   };
+//   var sort = {
+//     subject_name: 1
+//   }
+//   dbo.collection("classroom")
+//     .find(query)
+//     .sort(sort)
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//       db.close();
+//     });
+// });
+
+/////////////////////sort order////////////////////
+// MongoClient.connect(url, options, function (err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//   var query = { 
+//     subject_id: /^140/ 
+//   };
+//   var sort = {
+//     subject_name: -1
+//   }
+//   dbo.collection("classroom")
+//     .find(query)
+//     .sort(sort)
+//     .toArray(function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//       db.close();
+//     });
+// });
+
+
+
+
+  //////////////////insert data//////////////////////
+// MongoClient.connect(url, function (err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//   var newclass = {
+//     subject_id: 'aa140-391'
+//     , subject_name:'aaWeb UI'
+//     , room:  '1202B'
+//     , schedule:['Monday']
+//     , lecturer: "Korawit"
+//   };
+
+//   dbo.collection("classroom")
+//     .insertOne(newclass, function (err, res) {
+//       if (err) throw err;
+//       console.log("1 document inserted");
+//       db.close();
+//     });
+// });
+
+
+
+// ///////////////////////////update/////////////////////////////////////
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//     /////////Select target///////////////
+//   var myquery = { subject_id:'140-391'};
+//     /////////Set new value///////////////
+//   var newvalues = {$set: {room: '1303'} };
+//   dbo.collection("classroom").updateOne(myquery, newvalues, function(err, res) {
+//     //////////or updateOne or updateMany
+//     if (err) throw err;
+//     console.log(res.result.nModified + " document(s) updated");
+//     db.close();
+//   });
+// });
+
+///////////////////////////Delete/////////////////////////////////////
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//     /////////Select target///////////////
+//   var myquery = { subject_id:'977-277'};
+//   dbo.collection("classroom").deleteOne(myquery, function(err, res) {
+//     //////////or deleteOne or deleteMany
+//     if (err) throw err;
+//     console.log(res.result.nModified + " document(s) deleted");
+//     db.close();
+//   });
+// });
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("coc");
+//     /////////Select target///////////////
+//   var myquery = { subject_id:'aa140-391',subject_name:'aaWeb UI'};
+//   dbo.collection("classroom").deleteOne(myquery, function(err, res) {
+//     //////////or deleteOne or deleteMany
+//     if (err) throw err;
+//     console.log(res.result.nModified + " document(s) deleted");
+//     db.close();
+//   });
+// });
